25c97d471c30de7c4224e5e9934b3d708c729ab4
Hectorvelez09 <velezhector36h@gmail.com>
2024-05-01 22:30:32 -0500

    Estilos n

---
+++ js/index.js
@@ -0,0 +1,20 @@
+let teclas;
+let pantalla;
+
+window.onload = function() {
+    pantalla = document.getElementById('pantalla');
+    teclas = document.getElementsByClassName('tecla');
+    agregarEventos(teclas);
+    pantalla.value = '';
+}
+
+function agregarEventos(teclas){
+    for (let i = 0; i < teclas.length; i++) {
+        teclas[i].addEventListener('click', function(){
+            let valor = this.innerHTML;
+            
+                pantalla.value += valor;
+            
+        });
+    }
+}
